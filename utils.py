c89b849fe8a79507e85506dd2c2662254f8bc0ce
Antiqueeeee <antiqueserver4@gmail.com>
2023-11-14 18:02:30 +0800

    code upload

--- utils.py
+++ utils.py
@@ -5,3 +5,12 @@ def open_connection():
     graph = Graph(uri, auth=(username, password))
     return graph
 
+def text_input_properties_to_dict(properties):
+    _property = dict()
+    properties = properties.replace(":","：")
+    properties = properties.split("\n")
+    for prop in properties:
+        if len(prop):
+            key,value = prop.split("：")
+            _property[key] = value
+    return _property
