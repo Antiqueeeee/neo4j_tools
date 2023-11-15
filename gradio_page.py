4215c9def91e110e1064948ad384e041f9c856f6
Antiqueeeee <antiqueserver4@gmail.com>
2023-11-15 10:21:57 +0800

    code upload

--- gradio_page.py
+++ gradio_page.py
@@ -1,78 +1,11 @@
 import gradio
-from utils import open_connection,text_input_properties_to_dict
-from py2neo import Node,Relationship,NodeMatcher,RelationshipMatcher
-
-graph = open_connection()
+from utils import button_function_create_entity,button_function_create_relation 
+from utils import button_function_delete_entity,button_function_delete_relation,button_function_delete_spo
 
 
-
-# 添加
-def button_function_create_entity(new_entity_type,new_entity_properties):
-    node_matcher = NodeMatcher(graph)
-    _property = text_input_properties_to_dict(new_entity_properties)
-    nodes = node_matcher.match(new_entity_type).where(**_property)
-    if not len(nodes):
-        node = Node(new_entity_type,**_property)
-        graph.create(node)
-    return node_matcher.match(new_entity_type).where(**_property).first()
-
-def button_function_create_relation(
-        head_entity_type,head_entity_properties
-        ,tail_entity_type,tail_entity_properties
-        ,relation
-):
-    relation_matcher = RelationshipMatcher(graph)
-    head_entity = button_function_create_entity(head_entity_type,head_entity_properties)
-    tail_entity = button_function_create_entity(tail_entity_type,tail_entity_properties)
-    if not relation_matcher.match((head_entity,tail_entity), r_type=relation) and not relation_matcher.match((tail_entity,head_entity), r_type=relation):
-        relationship = Relationship(head_entity, relation, tail_entity)
-        graph.create(relationship)
-    return list(relation_matcher.match((head_entity,tail_entity), r_type=relation)) + list(relation_matcher.match((tail_entity,head_entity), r_type=relation))
-# 删除
-def button_function_delete_entity(entity_type,entity_properites):
-    node_matcher = NodeMatcher(graph)
-    relation_matcher = RelationshipMatcher(graph)
-    _property = text_input_properties_to_dict(entity_properites)
-    nodes = node_matcher.match(entity_type).where(**_property)
-    for node in nodes:
-        relationships = relation_matcher.match([node],r_type=None)
-        for relationship in relationships:
-            graph.delete(relationship)
-        graph.delete(node)
-def button_function_delete_relation(head_entity_type,head_entity_properties,tail_entity_type,tail_entity_properites,relation=None):
-    node_matcher = NodeMatcher(graph)
-    relation_matcher = RelationshipMatcher(graph)
-    _property = text_input_properties_to_dict(head_entity_properties)
-    head_entity = node_matcher.match(head_entity_type).where(**_property).first()
-    _property = text_input_properties_to_dict(tail_entity_properites)
-    tail_entity = node_matcher.match(tail_entity_type).where(**_property).first()
-    relationships = relation_matcher.match((head_entity,tail_entity),r_type=relation)
-    for relationship in relationships:
-        graph.separate(relationship)
-    relationships = relation_matcher.match((tail_entity,head_entity),r_type=relation)
-    for relationship in relationships:
-        graph.separate(relationship)
-def button_function_delete_spo(head_entity_type,head_entity_properites,tail_entity_type,tail_entity_properites):
-    node_matcher = NodeMatcher(graph)
-    relation_matcher = RelationshipMatcher(graph)
-    _property = text_input_properties_to_dict(head_entity_properites)
-    head_entity = node_matcher.match(head_entity_type).where(**_property).first()
-    _property = text_input_properties_to_dict(tail_entity_properites)
-    tail_entity = node_matcher.match(tail_entity_type).where(**_property).first()
-    relationships = relation_matcher.match((head_entity,tail_entity),r_type=None)
-    for relationship in relationships:
-        graph.delete(relationship)
-    relationships = relation_matcher.match((tail_entity,head_entity),r_type=None)
-    for relationship in relationships:
-        graph.delete(relationship)
 # button_function_create_relation("痤疮","疾病","胃炎","疾病","别名")
 # button_function_create_relation("痤疮","疾病","胃炎","疾病","名别")
 # button_function_create_relation("痤疮","疾病","胃炎","疾病","吃饭")
-
-# 修改
-def button_function_update_entity(entity_name,entity_type,new_name,new_type):
-    pass
-
 with gradio.Blocks() as demo:
     with gradio.Tab("增加"):
         gradio.Markdown("操作实体")
@@ -96,18 +29,19 @@ with gradio.Blocks() as demo:
             with gradio.Column():
                 text_input_entity_2_type = gradio.Text(label="实体2类型")
                 text_input_entity_2_properties = gradio.TextArea(label="实体2属性",value="name：")
-        with gradio.Row():
-            text_input_relation = gradio.Text(label="关系")
-            button_apply = gradio.Button("添加")
-            button_apply.click(
-                button_function_create_relation
-                ,inputs=[
-                    text_input_entity_1_type,text_input_entity_1_properties
-                    ,text_input_entity_2_type,text_input_entity_2_properties
-                    ,text_input_relation
-                ]
-                ,outputs=None
-            )
+            with gradio.Column():
+                text_input_relation = gradio.Text(label="关系")
+                text_input_relation_properties = gradio.TextArea(label="关系属性",value="name：")
+        button_apply = gradio.Button("添加")
+        button_apply.click(
+            button_function_create_relation
+            ,inputs=[
+                text_input_entity_1_type,text_input_entity_1_properties
+                ,text_input_entity_2_type,text_input_entity_2_properties
+                ,text_input_relation,text_input_relation_properties
+            ]
+            ,outputs=None
+        )
     with gradio.Tab("删除"):
         gradio.Markdown("操作实体")
         with gradio.Row():
@@ -124,28 +58,40 @@ with gradio.Blocks() as demo:
             with gradio.Column():
                 text_input_entity_2_type = gradio.Text(label="实体2类型")
                 text_input_entity_2_properties = gradio.TextArea(label="实体2属性",value="name：")
-        with gradio.Row():
-            text_input_relation = gradio.Text(label="关系")
-            button_apply = gradio.Button("删除")
+            with gradio.Column():
+                text_input_relation = gradio.Text(label="关系")
+                text_input_relation_properties = gradio.TextArea(label="关系属性")
+            with gradio.Row():
+                button_apply = gradio.Button("删除")
             button_apply.click(
                 button_function_delete_relation
-                ,inputs=[text_input_entity_1_type,text_input_entity_1_properties,text_input_entity_2_type,text_input_entity_2_properties]
+                ,inputs=[
+                    text_input_entity_1_type,text_input_entity_1_properties
+                    ,text_input_entity_2_type,text_input_entity_2_properties
+                    ,text_input_relation,text_input_relation_properties
+                ]
                 ,outputs=None
             )
         gradio.Markdown("操作三元组")
-        with gradio.Row():
-            text_input_entity_1_name = gradio.Text(label="头实体名称")
-            text_input_entity_1_type = gradio.Text(label="头实体类型")
-        with gradio.Row():
-            text_input_entity_2_name = gradio.Text(label="尾实体名称")
-            text_input_entity_2_type = gradio.Text(label="尾实体类型")
-        with gradio.Row():
-            button_apply = gradio.Button("删除")
-            button_apply.click(
-                button_function_delete_spo
-                ,inputs=[text_input_entity_1_name,text_input_entity_1_type,text_input_entity_2_name,text_input_entity_2_type]
-                ,outputs=None
-            )
+        with gradio.Column():
+            text_input_entity_1_type = gradio.Text(label="实体1类型")
+            text_input_entity_1_properties = gradio.TextArea(label="实体1属性",value="name：")
+        with gradio.Column():
+            text_input_entity_2_name = gradio.Text(label="实体2类型")
+            text_input_entity_2_type = gradio.TextArea(label="实体2属性",value="name：")
+        with gradio.Column():
+            text_input_relation = gradio.Text(label="关系")
+            text_input_relation_properties = gradio.TextArea(label="关系属性")
+        button_apply = gradio.Button("删除")
+        button_apply.click(
+            button_function_delete_spo
+            ,inputs=[
+                text_input_entity_1_type,text_input_entity_1_properties
+                ,text_input_entity_2_type,text_input_entity_2_properties
+                ,text_input_relation,text_input_relation_properties
+            ]
+            ,outputs=None
+        )
     with gradio.Tab("修改"):
         pass
 
--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
-
 def open_connection():
     from config import username,password,uri
     from py2neo import Graph
     graph = Graph(uri, auth=(username, password))
     return graph
 
+from py2neo import Node,Relationship,NodeMatcher,RelationshipMatcher
+graph = open_connection()
 def text_input_properties_to_dict(properties):
     _property = dict()
     properties = properties.replace(":","：")
@@ -14,3 +15,92 @@ def text_input_properties_to_dict(properties):
             key,value = prop.split("：")
             _property[key] = value
     return _property
+
+#
+#查询
+#
+def button_function_search_entity(entity_type,entity_properties):
+    node_matcher = NodeMatcher(graph)
+    _property = text_input_properties_to_dict(entity_properties)
+    nodes = node_matcher.match(entity_type).where(**_property)
+    if len(nodes):
+        return nodes.first()
+    else:
+        return None
+
+#
+# 添加
+#
+def button_function_create_entity(new_entity_type,new_entity_properties):
+    nodes = button_function_search_entity(new_entity_type,new_entity_properties)
+    if not len(nodes):
+        _property = text_input_properties_to_dict(new_entity_properties)
+        node = Node(new_entity_type,**_property)
+        graph.create(node)
+    return button_function_search_entity(new_entity_type,new_entity_properties)
+
+def button_function_create_relation(
+        head_entity_type,head_entity_properties
+        ,tail_entity_type,tail_entity_properties
+        ,relation,relation_properties
+):
+    relation_matcher = RelationshipMatcher(graph)
+    head_entity = button_function_create_entity(head_entity_type,head_entity_properties)
+    tail_entity = button_function_create_entity(tail_entity_type,tail_entity_properties)
+    _property = text_input_properties_to_dict(relation_properties)
+    if not relation_matcher.match((head_entity,tail_entity), r_type=relation,**_property) and not relation_matcher.match((tail_entity,head_entity), r_type=relation):
+        relationship = Relationship(head_entity, relation, tail_entity)
+        graph.create(relationship)
+    return list(relation_matcher.match((head_entity,tail_entity), r_type=relation)) + list(relation_matcher.match((tail_entity,head_entity), r_type=relation))
+# 
+# 删除
+#
+def button_function_delete_entity(entity_type,entity_properites):
+    relation_matcher = RelationshipMatcher(graph)
+    nodes = button_function_create_entity(entity_type,entity_properites)
+    for node in nodes:
+        # 接触与该节点相关的所有关系
+        # 然后删除该节点
+        relationships = relation_matcher.match([node],r_type=None)
+        for relationship in relationships:
+            graph.separate(relationship)
+        graph.delete(node)
+def button_function_delete_relation(
+        head_entity_type,head_entity_properties
+        ,tail_entity_type,tail_entity_properites
+        ,relation=None,relation_properties=""
+):
+    relation_matcher = RelationshipMatcher(graph)
+
+    head_entity = button_function_create_entity(head_entity_type,head_entity_properties)
+    tail_entity = button_function_create_entity(tail_entity_type,tail_entity_properites)
+    _property = text_input_properties_to_dict(relation_properties)
+    relationships = relation_matcher.match((head_entity,tail_entity),r_type=relation,**_property)
+    for relationship in relationships:
+        graph.separate(relationship)
+    relationships = relation_matcher.match((tail_entity,head_entity),r_type=relation,**_property)
+    for relationship in relationships:
+        graph.separate(relationship)
+def button_function_delete_spo(
+        head_entity_type,head_entity_properites
+        ,tail_entity_type,tail_entity_properites
+        ,text_input_relation=None,text_input_relation_properties=""
+):
+    relation_matcher = RelationshipMatcher(graph)
+    head_entity = button_function_create_entity(head_entity_type,head_entity_properites)
+    tail_entity = button_function_create_entity(tail_entity_type,tail_entity_properites)
+    # 考虑两个方向
+    _property = text_input_properties_to_dict(text_input_relation_properties)
+    relationships = relation_matcher.match((head_entity,tail_entity),r_type=text_input_relation,**_property)
+    for relationship in relationships:
+        graph.delete(relationship)
+    relationships = relation_matcher.match((tail_entity,head_entity),r_type=text_input_relation,**_property)
+    for relationship in relationships:
+        graph.delete(relationship)
+
+# 修改
+def button_function_update_entity(
+                entity_type,entity_properties
+                ,new_type,new_properties
+):
+    pass
